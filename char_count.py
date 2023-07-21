67c681887341409b8bb18367f1b742fee3ad06da
adnannshaikh <101106625+adnannshaikh@users.noreply.github.com>
2023-07-21 20:38:18 +0530

    Add files via upload

---
+++ char_count.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name : ")
+age = int(input("Please enter your age : "))
+cal = 100 - age
+hund_age = 2023 +(age+cal)
+
+print(f"your name is {name} and your age is {age} and \nwhen you'll be 100 the year will be {hund_age}")
